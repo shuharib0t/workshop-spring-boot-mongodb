fb7a06a1a69ead104bb9d93c180422f464834842
Julio Ishikawa <japaishikawa@gmail.com>
2022-02-16 19:39:14 -0300

    Obtendo um usuário por ID

--- src/main/java/com/julio/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/julio/workshopmongo/repository/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.julio.workshopmongo.domain.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{
+public interface UserRepository extends MongoRepository<User, String> {
 
-	
 }
