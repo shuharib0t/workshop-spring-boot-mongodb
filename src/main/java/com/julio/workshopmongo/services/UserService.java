31f2e01238607fdca0b2e47cf0fe22561dd5e759
Julio Ishikawa <japaishikawa@gmail.com>
2022-02-21 08:55:26 -0300

    Inserção de usuário com POST

--- src/main/java/com/julio/workshopmongo/services/UserService.java
+++ src/main/java/com/julio/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.julio.workshopmongo.domain.User;
+import com.julio.workshopmongo.dto.UserDTO;
 import com.julio.workshopmongo.repository.UserRepository;
 import com.julio.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,13 @@ public class UserService {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 		return user;
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
