03851f6d27dc89060142d60d0c1564ab7626c424
Julio Ishikawa <japaishikawa@gmail.com>
2022-03-05 13:39:15 -0300

    Obtendo post por id

---
+++ src/main/java/com/julio/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.julio.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.julio.workshopmongo.domain.Post;
+import com.julio.workshopmongo.repository.PostRepository;
+import com.julio.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+		
+	public Post findById(String id) {
+		Post user = repo.findById(id).orElse(null);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+}
